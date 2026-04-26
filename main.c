3f776072cd4b7a05d979085313df41bd86a75081
LeonByte <153441621+LeonByte@users.noreply.github.com>
2026-04-26 19:09:55 +0200

    add students and scores

--- main.c
+++ main.c
@@ -1,2 +1,21 @@
 //Write your code in this file
 
+#include <stdio.h>
+
+#define STUDENTS 5
+#define TESTS 13
+
+int main(){
+    char name[STUDENTS][11];
+    int score[STUDENTS][TESTS];
+
+    for (int i = 0; i< TESTS; i++){
+        scanf("%s", name[i]);
+
+        for (int j =0; j < TESTS; j++){
+            scanf("%d" , &score[i][j]);
+        }
+    }
+
+    return 0;
+}
